ec63e9869f0572b10821f69ceb21110780e583c6
Julius <juliusastein@gmail.com>
2022-10-13 15:30:01 -0400

    working model

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # todo-flask
 A simple to-do app built and hosted with Flask
+
+To run this application locally, you will need to run the following commands:
+
+```bash
+$ pip install Flask
+$ pip install Flask-SQLAlchemy
+```
+
+Next, set the following environment variables on your local machine:
+
+```bash
+$ FLASK_APP=todo.py
+$ FLASK_ENV=development
+```
+
+Finally, run the following command to start the application:
+
+```bash
+$ python app.py
+```
--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.app_context().push()
 db = SQLAlchemy(app)
 
 # This is the model for the database table that will be created by SQLAlchemy 
@@ -22,6 +23,30 @@ def home():
     todo_list = Todo.query.all()
     return render_template("base.html", todo_list=todo_list)
 
+# This route is for adding new items to the todo list
+@app.route("/add", methods=["POST"])
+def add():
+    title = request.form.get("title")
+    new_todo = Todo(title=title, complete=False)
+    db.session.add(new_todo)
+    db.session.commit()
+    return redirect(url_for('home'))
+
+# This route is for toggling the complete variable
+@app.route("/update/<int:todo_id>")
+def update(todo_id):
+    todo = Todo.query.filter_by(id=todo_id).first()
+    todo.complete = not todo.complete
+    db.session.commit()
+    return redirect(url_for("home"))
+
+# This route is for deleting items from the todo list
+@app.route("/delete/<int:todo_id>")
+def delete(todo_id):
+    todo = Todo.query.filter_by(id=todo_id).first()
+    db.session.delete(todo)
+    db.session.commit()
+    return redirect(url_for('home'))
 
 @app.route("/about")
 def about():
@@ -32,9 +57,9 @@ if __name__ == "__main__":
     db.create_all()
     
     #- Demo - Adding a todo
-    new_todo = Todo(text="This is a new todo", complete=False)
-    db.session.add(new_todo)
-    db.session.commit()
+    # new_todo = Todo(title="This is a new todo", complete=False)
+    # db.session.add(new_todo)
+    # db.session.commit()
 
     # This line starts the development server with the run() method. 
     # The debug parameter is set to True, which will activate the debugger and reloader. 
